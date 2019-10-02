5a572ec65e3240883fe0fc391fd344b7bfe568be
Maria Gabriella Brodi <50110012+mgbrodi@users.noreply.github.com>
2019-10-02 09:56:29 -0400

    Always Recreate DB

--- ModelsCommons/Model/TickerDatabaseInit.cs
+++ ModelsCommons/Model/TickerDatabaseInit.cs
@@ -20,7 +20,7 @@ using System.Web;
 
 namespace ModelsCommons.Model
 {
-    public class TickerDatabaseInit : DropCreateDatabaseIfModelChanges<TickerContext>
+    public class TickerDatabaseInit : DropCreateDatabaseAlways<TickerContext>
     {
         protected override void Seed(TickerContext context)
         {
